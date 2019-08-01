62f338a10f769045e6d923d97bfeb149a907c5fe
RumblingBee <m.dezette@gmail.com>
2019-08-01 20:40:27 +0200

    Création d'un salon de raid dans la categorie raid

--- main/channelManager.js
+++ main/channelManager.js
@@ -1,11 +1,25 @@
 exports.createChannel = function (args, msg, client) {
     var server = msg.guild;
-    var channelName = args[0];
+
+    const pokemonName = args[0];
+    const arenaName = args[1];
+    const time = args[2];
+    let channelName;
+
+
+    if (time[0] === '@') {
+
+    } else {
+        channelName = "0-" + pokemonName + "-" + arenaName + "-" + time;
+    }
     var channel;
+
+    console.log(client);
 
     server.createChannel(channelName, "text").then(() => {
 
         channel = client.channels.find("name", channelName);
+        channel.setParent("606532668313567262");
         channel.send("C'est parti pour un raid!");
 
         setTimeout(function () {
